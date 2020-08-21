d2bc0ac27938db3a14f949bc232a4a6e90d26ae9
prioarief <prioariefgunawangunawan@gmail.com>
2020-08-21 14:06:34 +0700

    remove logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
-import { persistStore } from 'redux-persist'
-import promiseMiddleware from 'redux-promise-middleware'
-import rootReducer from './reducers/index'
+import { applyMiddleware, createStore } from 'redux';
+import { persistStore } from 'redux-persist';
+import promiseMiddleware from 'redux-promise-middleware';
+import rootReducer from './reducers/index';
 
 // export default createStore(
 // 	rootReducer,
 // 	applyMiddleware(promiseMiddleware, logger)
 // )
 
-const store = createStore(
-	rootReducer,
-	applyMiddleware(promiseMiddleware, logger)
-)
+const store = createStore(rootReducer, applyMiddleware(promiseMiddleware));
 
-const persistor = persistStore(store)
+const persistor = persistStore(store);
 
-export default { store, persistor }
+export default { store, persistor };
